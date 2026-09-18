e858b855f741c374b13734f0455dca3851013cda
mahdi-egh <mahdieghdamdoust@gmail.com>
2026-09-19 01:33:53 +0330

    insert keyboard

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ import sys
 import sdkHelper
 helper = sdkHelper.sdkHelper()
 def main():
+    if len(sys.argv)<2:
+        print("missing method")
+        return
     inputRequest = sys.argv[1]
-    print(sys.argv[2] if len(sys.argv)>2 else "")
+    
     token = "696988573:zwApZSZMLYtkRdgHECpYOkuU8NchkYgVjbE"
+
     match inputRequest:
-        case sendMessage:
-            helper.sendMessage(token,chat_id,text)
-    helper.sendMessage(token,"1786476947",'hiiiii')
+        case "sendMessage":
+            chat_id = sys.argv[2]
+            text = sys.argv[3]
+            requst = helper.sendMessage(token,chat_id,text)
+            print(requst)
+            
+        case "getMe":
+            print(helper.getMe(token))
+
+        case "getUpdates":
+            print(helper.getUpdates(token))
 if __name__ == '__main__':
     main()
-# things that i must do:
-    # get data by argv
-    # remake match case 
--- sdkHelper.py
+++ sdkHelper.py
@@ -1,16 +1,42 @@
 import requests
 class sdkHelper:
     url = "https://tapi.bale.ai/bot"
-    def printName(self,name = "stranger"):
-        print(f"hello dear {name}")
+    entryKeyboard = {
+        "keyboard" : [
+            [{"text":"شروع"}]
+        ],"resize_keyboard":True
+    }
+    def getMe(self,token):
+        if len(token) == 0:
+            return "token is missing"
+        # validate token
+        response = requests.post(f"{self.url}{token}/getMe").json()
+        if not response['ok']:
+            return "invalid token"
+        return(response['result'])
 
-    def w3scholls(self):
-        x = requests.get("https://www.w3schools.com/python/demopage.php")
-        print(x.text)
-    def sendMessage(self,token,chat_id,text = ""):
-        x = requests.get(f"{self.url}{token}/sendMessage",json = {'chat_id' : chat_id ,'text' : text})
-        print(x)
+    def sendMessage(self,token,chat_id,text,reply_markup = None):
+        if not token:
+            return "token is missing"
+        if not chat_id:
+            return "chat id is missing"
+        if not text:
+            return "text id is missing"
+        response = requests.get(f"{self.url}{token}/sendMessage",json={"chat_id": chat_id,"text": text,"reply_markup": reply_markup}).json()
+        return response
+        
     def getUpdates(self, token):
-        response = requests.get(f"{self.url}{token}/getUpdates")
-        return response.json()
-    
+        if len(token) < 1:
+            return "token is missing"
+        response = requests.get(f"{self.url}{token}/getUpdates").json()
+        updates_id = [item['update_id'] for item in response['result']]
+        message = [item['message'] for item in response['result']]
+        messageFrom = [item['from'] for item in message]
+        for item in message:
+             if 'text' in item:
+                 if item['text'] == '/start':
+                     if item['from']['id']:
+                         print(
+                             self.sendMessage(token,item['from']['id'],"سلام 👋",self.entryKeyboard))
+
+            
